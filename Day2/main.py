8f66e4920851515d9bd48e1b9866111a76249144
MatiBarcia <matiasalejandrobarcia@gmail.com>
2024-07-31 23:54:10 -0300

    Part 1 Working

--- Day2/main.py
+++ Day2/main.py
@@ -1,10 +1,47 @@
-# ACORDATE DEL NUMERO DE GAME
+import re
 
-with open("Day2/input.txt", "r") as f:
-    line = f.readline()
-    line.find(":")
-    lineSplit = line[line.find(":")+1:]
-    lineSplit = lineSplit.split(";") # usa mas variables paja
+fileName = 'input.txt'
+numGames = sum(1 for _ in open(fileName))
 
-    for line in lineSplit:
-        print(line)
+def part1():
+    with open(fileName, "r") as f:
+        sum = 0
+        for id in range(1, numGames+1):
+            line = f.readline()
+            # print("Game ", end=str(id)+"\n")
+
+            gameSplit = line[line.find(":")+1:]
+            setSplit = gameSplit.split(";")
+            valid = True
+
+            for set in setSplit:
+                colorSplit = set.split(",")
+                for color in colorSplit:
+                    if((color.find("red") != -1) & valid):
+                        amountRed = re.search(r'\d+', color).group()
+                        if(int(amountRed) > 12):
+                            valid = False
+                            break
+
+                    if((color.find("green") != -1) & valid):
+                        amountGreen = re.search(r'\d+', color).group()
+                        if(int(amountGreen) > 13):
+                            valid = False
+                            break
+                    
+                    if((color.find("blue") != -1) & valid):
+                        amountBlue = re.search(r'\d+', color).group()
+                        if(int(amountBlue) > 14):
+                            valid = False
+                            break
+            
+            if(valid):
+                sum += id
+                # print("Se suma ", end=str(id)+"\n")
+        
+        return sum
+    
+def part2():
+    
+
+print(part1())
