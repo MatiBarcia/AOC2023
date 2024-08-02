6c84a7414183dbb5cf46dad9c638f34de903fd24
MatiBarcia <matiasalejandrobarcia@gmail.com>
2024-08-02 01:16:54 -0300

    Part 1 working

--- Day3/main.py
+++ Day3/main.py
@@ -1,97 +1,97 @@
 import re
 
-fileName = 'input.txt'
-symbols = ['*', '+', '%', '&', '$', '-', '=', '/', '#', '@']
+fileName = "input.txt"
+symbols = ["*", "+", "%", "&", "$", "-", "=", "/", "#", "@"]
 sum = 0
 
 with open(fileName, "r") as f:
-    previousLine = f.readline() # primera linea nunca tiene simbolo
+    previousLine = f.readline()  # primera linea nunca tiene simbolo
     currentLine = f.readline()
     followingLine = f.readline()
 
     while followingLine != "":
         for i, currentChar in enumerate(currentLine):
             if currentChar in symbols:
-                if(previousLine[i].isdigit()):
+                if previousLine[i].isdigit():
                     before = False
                     after = False
-                    if(previousLine[i-1].isdigit()): 
+                    if previousLine[i - 1].isdigit():
                         before = True
-                    if(previousLine[i+1].isdigit()):
+                    if previousLine[i + 1].isdigit():
                         after = True
-                    if(before and after):
-                        numero = int(previousLine[i-1:i+2])
+                    if before and after:
+                        numero = int(previousLine[i - 1 : i + 2])
                         print(numero)
                         sum += numero
-                    elif(before):
-                        newLine = previousLine[:i+1]
-                        numero = int((re.search(r'\d+', newLine[::-1]).group())[::-1])
+                    elif before:
+                        newLine = previousLine[: i + 1]
+                        numero = int((re.search(r"\d+", newLine[::-1]).group())[::-1])
                         print(numero)
                         sum += numero
-                    elif(after):
+                    elif after:
                         newLine = previousLine[i:]
-                        numero = int(re.search(r'\d+', newLine).group())
+                        numero = int(re.search(r"\d+", newLine).group())
                         print(numero)
                         sum += numero
                     else:
                         sum += int(previousLine[i])
 
-                elif(previousLine[i-1].isdigit()):
+                elif previousLine[i - 1].isdigit():
                     newLine = previousLine[:i]
-                    numero = int((re.search(r'\d+', newLine[::-1]).group())[::-1])
+                    numero = int((re.search(r"\d+", newLine[::-1]).group())[::-1])
                     print(numero)
                     sum += numero
 
-                if(not previousLine[i].isdigit() and previousLine[i+1].isdigit()):
-                    newLine = previousLine[i+1:]
-                    numero = int(re.search(r'\d+', newLine).group())
+                if not previousLine[i].isdigit() and previousLine[i + 1].isdigit():
+                    newLine = previousLine[i + 1 :]
+                    numero = int(re.search(r"\d+", newLine).group())
                     print(numero)
                     sum += numero
-                
-                if(currentLine[i-1].isdigit()):
+
+                if currentLine[i - 1].isdigit():
                     newLine = currentLine[:i]
-                    numero = int((re.search(r'\d+', newLine[::-1]).group())[::-1])
+                    numero = int((re.search(r"\d+", newLine[::-1]).group())[::-1])
                     print(numero)
                     sum += numero
-                if(currentLine[i+1].isdigit()):
-                    newLine = currentLine[i+1:]
-                    numero = int(re.search(r'\d+', newLine).group())
+                if currentLine[i + 1].isdigit():
+                    newLine = currentLine[i + 1 :]
+                    numero = int(re.search(r"\d+", newLine).group())
                     print(numero)
                     sum += numero
 
-                if(followingLine[i].isdigit()):
+                if followingLine[i].isdigit():
                     before = False
                     after = False
-                    if(followingLine[i-1].isdigit()): 
+                    if followingLine[i - 1].isdigit():
                         before = True
-                    if(followingLine[i+1].isdigit()):
+                    if followingLine[i + 1].isdigit():
                         after = True
-                    if(before and after):
-                        numero = int(followingLine[i-1:i+2])
+                    if before and after:
+                        numero = int(followingLine[i - 1 : i + 2])
                         print(numero)
                         sum += numero
-                    elif(before):
-                        newLine = followingLine[:i+1]
-                        numero = int((re.search(r'\d+', newLine[::-1]).group())[::-1])
+                    elif before:
+                        newLine = followingLine[: i + 1]
+                        numero = int((re.search(r"\d+", newLine[::-1]).group())[::-1])
                         print(numero)
                         sum += numero
-                    elif(after):
+                    elif after:
                         newLine = followingLine[i:]
-                        numero = int(re.search(r'\d+', newLine).group())
+                        numero = int(re.search(r"\d+", newLine).group())
                         print(numero)
                         sum += numero
                     else:
                         sum += int(followingLine[i])
-                        
-                elif(followingLine[i-1].isdigit()):
+
+                elif followingLine[i - 1].isdigit():
                     newLine = followingLine[:i]
-                    numero = int((re.search(r'\d+', newLine[::-1]).group())[::-1])
+                    numero = int((re.search(r"\d+", newLine[::-1]).group())[::-1])
                     print(numero)
                     sum += numero
 
-                if(not followingLine[i].isdigit() and followingLine[i+1].isdigit()):
-                    newLine = followingLine[i+1:]
-                    numero = int(re.search(r'\d+', newLine).group())
+                if not followingLine[i].isdigit() and followingLine[i + 1].isdigit():
+                    newLine = followingLine[i + 1 :]
+                    numero = int(re.search(r"\d+", newLine).group())
                     print(numero)
                     sum += numero
 
